a41464365907e40571deb20fc4c93562fc9e4887
Farad2020 <fnirc@mail.ru>
2021-07-12 00:21:06 +0600

    seedData loading attacker loadouts and views upd

--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -16,7 +16,7 @@ namespace R6MIX.Models
                     DbContextOptions<Data.ApplicationDbContext>>()))
             {
                 seedSides(context);
-
+                seedLoadouts(context);
 
                 context.SaveChanges();
             }
@@ -43,6 +43,74 @@ namespace R6MIX.Models
                     {
                         Name = "Defender",
                         Icon = "/media/general/defender.PNG",
+                    }
+                );
+            }
+
+            void seedLoadouts(Data.ApplicationDbContext context)
+            {
+                // Look for any Side.
+                /*
+                if (context.Side.Any())
+                {
+                    return;   // DB has been seeded
+                }
+                */
+                foreach (var entity in context.Loadout)
+                    context.Loadout.Remove(entity);
+
+                context.Loadout.AddRange(
+                    // Filling Attacker Loadouts
+                    new Loadout
+                    {
+                        Name = "AshLoadout",
+                        PrimaryWeapon = "R4-C",
+                        PrimaryWeaponImg = "/media/loadoutImgs/attackerLoadouts/primary/R4-C.webp",
+                        SecondaryWeapon = "5.7 USG",
+                        SecondaryWeaponImg = "/media/loadoutImgs/attackerLoadouts/secondary/5.7_USG.webp",
+                        Gadget = "Claymore",
+                        GadgetImg = "/media/loadoutImgs/attackerLoadouts/gadget/Claymore.webp",
+
+                    },
+                    new Loadout
+                    {
+                        Name = "ThermiteLoadout",
+                        PrimaryWeapon = "556XI",
+                        PrimaryWeaponImg = "/media/loadoutImgs/attackerLoadouts/primary/556xi.webp",
+                        SecondaryWeapon = "5.7 USG",
+                        SecondaryWeaponImg = "/media/loadoutImgs/attackerLoadouts/secondary/5.7_USG.webp",
+                        Gadget = "Smoke Grenade",
+                        GadgetImg = "/media/loadoutImgs/attackerLoadouts/gadget/Smoke_Grenade.webp",
+                    },
+                    new Loadout
+                    {
+                        Name = "ZeroLoadout",
+                        PrimaryWeapon = "MP7",
+                        PrimaryWeaponImg = "/media/loadoutImgs/attackerLoadouts/primary/MP7.webp",
+                        SecondaryWeapon = "GONNE-6",
+                        SecondaryWeaponImg = "/media/loadoutImgs/attackerLoadouts/secondary/GONNE-6.webp",
+                        Gadget = "Claymore",
+                        GadgetImg = "/media/loadoutImgs/attackerLoadouts/gadget/Claymore.webp",
+                    },
+                    new Loadout
+                    {
+                        Name = "DokkaebiLoadout",
+                        PrimaryWeapon = "MK 14 EBR",
+                        PrimaryWeaponImg = "/media/loadoutImgs/attackerLoadouts/primary/Mk_14_EBR.webp",
+                        SecondaryWeapon = "SMG-12",
+                        SecondaryWeaponImg = "/media/loadoutImgs/attackerLoadouts/secondary/SMG-12.webp",
+                        Gadget = "Stun Grenade",
+                        GadgetImg = "/media/loadoutImgs/attackerLoadouts/gadget/Stun_Grenade.webp",
+                    },
+                    new Loadout
+                    {
+                        Name = "HibanaLoadout",
+                        PrimaryWeapon = "TYPE-89",
+                        PrimaryWeaponImg = "/media/loadoutImgs/attackerLoadouts/primary/Type-89.webp",
+                        SecondaryWeapon = "BEARING 9",
+                        SecondaryWeaponImg = "/media/loadoutImgs/attackerLoadouts/secondary/Bearing_9.webp",
+                        Gadget = "Breache Charge",
+                        GadgetImg = "/media/loadoutImgs/attackerLoadouts/gadget/Breache_Charge.webp",
                     }
                 );
             }
