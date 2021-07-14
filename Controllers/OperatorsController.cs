89f2f81c92462dff82c6eacdba8d82df8707701b
Farad2020 <fnirc@mail.ru>
2021-07-14 23:57:36 +0600

    home index upd

--- Controllers/OperatorsController.cs
+++ Controllers/OperatorsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -43,6 +44,7 @@ namespace R6MIX.Controllers
             return View(@operator);
         }
 
+        [Authorize]
         // GET: Operators/Create
         public IActionResult Create()
         {
@@ -66,6 +68,7 @@ namespace R6MIX.Controllers
         }
 
         // GET: Operators/Edit/5
+        [Authorize]
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -117,6 +120,7 @@ namespace R6MIX.Controllers
         }
 
         // GET: Operators/Delete/5
+        [Authorize]
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
